74f88753e9e25dee6cd5c28682bc02f428b27c6b
JPerezD10 <jperezdemonty@live.com.ar>
2023-10-03 10:21:50 -0300

    Fix UI removing MetroForm

---
+++ Supermercado-Juarez-Antiñolo/UI/ServiceForm.cs
@@ -0,0 +1,24 @@
+﻿using Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI
+{
+    public class ServiceForm : Form
+    {
+        protected void RevisarRespuestaServicio<T>(BusinessRespuesta<T> respuesta)
+        {
+            if (!respuesta.Ok)
+            {
+                MessageBox.Show(respuesta.Mensaje, "Epa!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            else if (!string.IsNullOrEmpty(respuesta.Mensaje))
+                MessageBox.Show(respuesta.Mensaje, "Bien!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
